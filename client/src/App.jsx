0ec2e49f54714e0d895a46b15d970804ab63753b
Imon <contact@imonweb.co.uk>
2023-07-02 23:09:56 +0100

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,10 @@ import {
   useLocation
 } from 'react-router-dom';
 import Home from "./scenes/home/Home"
+import ItemDetails from './scenes/itemDetails/itemDetails';
+import { Checkbox } from '@mui/material';
+import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/Navbar';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -22,9 +26,13 @@ function App() {
   return (
       <div className='app'>
         <BrowserRouter >
+          <Navbar />
           <ScrollToTop />
             <Routes>
               <Route path="/" element={ <Home /> } />
+              <Route path="item/:itemId" element={ <ItemDetails /> } />
+              <Route path="checkout" element={<Checkbox />} />
+              <Route path="checkout/success" element={<Confirmation />} />
             </Routes>
         </BrowserRouter>
       </div>
